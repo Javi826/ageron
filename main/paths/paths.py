d6b1db1b69f6ac316ec7c8a936fd25e31e77e435
Javi826 <jlahoz.ferrandez@gmail.com>
2024-01-24 21:10:19 +0100

    24-01

--- main/paths/paths.py
+++ main/paths/paths.py
@@ -6,24 +6,25 @@ Created on Sat Dec 30 21:06:19 2023
 
 import os
 
-# Directory to store CSV files
-path_base = r"C:\Users\javier\Desktop\ML\ageron"
+## Directorio para almacenar archivos CSV
+path_base = "/Users/javi/Desktop/ML/ageron"
 
 file_df_data = "sp500_data.csv"
-folder_csv = "inputs\historicyh"
-path_file_csv = os.path.join(path_base,folder_csv,file_df_data)
+folder_csv = "inputs/historicyh"
+path_file_csv = os.path.join(path_base, folder_csv, file_df_data)
 
 file_df_data_clean = "df_data_clean.csv"
-folder_df_data_clean = "inputs\dtset_clean"
-path_df_data_clean = os.path.join(path_base,folder_df_data_clean, file_df_data_clean)
+folder_df_data_clean = "inputs/dtset_clean"
+path_df_data_clean = os.path.join(path_base, folder_df_data_clean, file_df_data_clean)
 
 file_summary_stats = 'df_summary_stats'
-folder_summary_stats = "outputs\\summary_stats"
-path_summary_stats= os.path.join(path_base,folder_summary_stats, file_summary_stats)
+folder_summary_stats = "outputs/summary_stats"
+path_summary_stats = os.path.join(path_base, folder_summary_stats, file_summary_stats)
 
 file_preprocessing = 'df_preprocessing.xlsx'
-folder_preprocessing = "outputs\\preprocessing"
-path_preprocessing = os.path.join(path_base,folder_preprocessing, file_preprocessing)
+folder_preprocessing = "outputs/preprocessing"
+path_preprocessing = os.path.join(path_base, folder_preprocessing, file_preprocessing)
+
 
 
 
